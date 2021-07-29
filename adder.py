3f1e94ab28a480d4e87fe5fb692be353d9329bef
hackermachan <88127923+Hackermachan2@users.noreply.github.com>
2021-07-29 09:18:44 +0000

    Update adder.py

--- adder.py
+++ adder.py
@@ -20,8 +20,8 @@ print (gr+" ║ ├┤ │  ├┤ ║ ╦  ╠═╣ ││ ││├┤ ├┬
 print (re+" ╩ └─┘┴─┘└─┘╚═╝  ╩ ╩─┴┘─┴┘└─┘┴└─")
 
 print (cy+"version : 1.01")
-print (cy+"Make sure you Subscribed Termux Professor On Youtube")
-print (cy+"www.youtube.com/c/TermuxProfessorYT")
+print (cy+"Make sure you Subscribed hackermachan On Youtube")
+print (cy+"https://www.youtube.com/channel/UC5AmiT44HyEZ2Rkc0WT0ZZA")
 
 print (re+"NOTE :")
 print ("1. Telegram only allow to add 200 members in group by one user.")
